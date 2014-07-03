ffb6ec56308f380f1d83ef5e6a89488d36b0484c
Igor Maculan <igor.maculan@infocamere.it>
2014-07-03 19:01:17 +0200

    add log parser feature

--- pylog4all/client.py
+++ pylog4all/client.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import json
 from optparse import OptionParser, OptionGroup
 import os
@@ -62,20 +61,60 @@ def user_setup():
     write_user_conf(conf)
 
 
+def get_level_from_string(log_file_line):
+    if 'DEBUG' in log_file_line:
+        return 'DEBUG'
+    if 'INFO' in log_file_line:
+        return 'INFO'
+    if 'WARN' in log_file_line:
+        return 'WARN'
+    if 'ERROR' in log_file_line:
+        return 'ERROR'
+    if 'FATAL' in log_file_line:
+        return 'FATAL'
+    return None
+
+
+def add_log_line(log4all_client,application, log_file_line):
+    global options
+    log_stack = None
+    log_level = options.level
+    log_line = None
+    if log_file_line[0] != '\t':
+        # new log line
+        log_line = log_file_line
+        log_level = get_level_from_string(log_file_line)
+        if log_level is None:
+            log_level = options.level
+        log_stack = None
+    else:
+        # probable stack trace
+        if log_stack is None:
+            log_stack = [log_file_line]
+        else:
+            log_stack.append(log_file_line)
+    success, error = log4all_client.add_log(application, log_level, log_line, log_stack)
+    if not success:
+        if options.verbose:
+            print("Log not added:" + error)
+
+
 def main():
-    parser = OptionParser()
+    usage = "usage: %prog [options] log_text "
+    parser = OptionParser(usage=usage)
     parser.add_option("--user-setup", dest="user_setup", action="store_true", help="Setup user configuration")
     parser.add_option("-s", "--log4all-server", dest="server", action="store", help="log4all server url")
     parser.add_option("-a", "--application", dest="application", action="store", help="log4all application")
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="show debug information")
-    option_level_group = OptionGroup(parser, 'Levels')
+    parser.add_option("-l", "--log-file", dest="log_file", action="store", help="log4 file to add")
+    option_level_group = OptionGroup(parser, 'Levels(*)', 'Level is mandatory')
     option_level_group.add_option('-d', '--debug', dest='level', action='store_const', const='DEBUG')
     option_level_group.add_option('-i', '--info', dest='level', action='store_const', const='INFO')
     option_level_group.add_option('-w', '--warn', dest='level', action='store_const', const='WARN')
     option_level_group.add_option('-e', '--error', dest='level', action='store_const', const='ERROR')
     option_level_group.add_option('-f', '--fatal', dest='level', action='store_const', const='FATAL')
     parser.add_option_group(option_level_group)
-
+    global options
     (options, args) = parser.parse_args(sys.argv)
     if options.user_setup:
         user_setup()
@@ -93,32 +132,50 @@ def main():
 
         if server.strip() == '':
             print ('server is mandatory')
+            parser.print_help()
             exit(-2)
 
         if application.strip() == '':
             print ('application is mandatory')
+            parser.print_help()
             exit(-2)
 
         if options.level is None:
             print ('level is mandatory')
+            parser.print_help()
+            exit(-2)
+
+        if options.log_file is not None and not sys.stdin.isatty():
+            print ('--log_file options is not compatible with pipe')
+            parser.print_help()
             exit(-2)
 
         cl = Log4allClient(server)
-
         # check if is piped
+        success, error = False, ''
         if not sys.stdin.isatty():
-            stack = sys.stdin.readlines()
+            stdin_lines = sys.stdin.readlines()
+            stack = [line.rstrip() for line in stdin_lines]
+            success, error = cl.add_log(application, options.level, args[1], stack)
+            if not success:
+                if options.verbose:
+                    print("Log not added:" + error)
+                sys.exit(-1)
         else:
-            stack = None
-
-        success, error = cl.add_log(application, options.level, args[1], stack)
-        if success:
-            if options.verbose:
-                print("Log added")
-        else:
-            if options.verbose:
-                print("Log not added:" + error)
-            sys.exit(-1)
+            if options.log_file is not None:
+                log_file = open(options.log_file)
+                log_file_lines = log_file.readlines()
+                try:
+                    for log_file_line in log_file_lines:
+                        add_log_line(cl, application, log_file_line)
+                finally:
+                    log_file.close()
+            else:
+                success, error = cl.add_log(application, options.level, args[1], None)
+                if not success:
+                    if options.verbose:
+                        print("Log not added:" + error)
+                    sys.exit(-1)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from distutils.core import setup
 setup(
     name='pyLog4all',
     version='0.0.1',
-    packages=[''],
+    packages=['pylog4all'],
     url='http://n3wtron.github.io/log4all/',
     license='GPL v3',
     author='Igor Maculan',
     author_email='n3wtron@gmail.com',
-    description='log4all python client'
+    description='log4all python client',
+    scripts=[
+        'scripts/l4a'
+    ]
 )
